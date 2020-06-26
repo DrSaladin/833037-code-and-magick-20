ad4f66ba9e3915e8181c9daa20feecd6c7c9affe
DrVitalii <dr.saladinn@yandex.ru>
2020-06-26 19:15:13 +0700

    Splitted up modules, fixed wizard customization function, and renamed modules

--- js/custom-wizard.js
+++ js/custom-wizard.js
@@ -5,34 +5,43 @@
   var inputWizardCoat = wizardSetup.querySelector('input[name = "coat-color"]');
   var wizardCoat = wizardSetup.querySelector('.wizard-coat');
 
-  var fireballColor = wizardSetup.querySelector('.setup-fireball-wrap');
   var fireball = wizardSetup.querySelector('.setup-fireball');
   var inputFireball = wizardSetup.querySelector('input[name = "fireball-color"]');
 
   var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
   var inputWizardEyes = wizardSetup.querySelector('input[name = "eyes-color"]');
 
-  var onElementPress = function (elementTitle, array, elementInput) {
-    if (elementTitle.tagName === 'DIV') {
-      elementTitle.style.backgroundColor = window.utils.getArrayRandElement(array);
-      elementInput.value = elementTitle.style.backgroundColor;
+
+  var onElementPress = function (evt, array, elementInput) {
+    if (evt.target.tagName === 'use') {
+      evt.target.style.fill = window.utils.getArrayRandElement(array);
+      elementInput.value = evt.target.style.fill;
     }
-    elementTitle.style.fill = window.utils.getArrayRandElement(array);
-    elementInput.value = elementTitle.style.fill;
-    return function () {
-      onElementPress(elementTitle, array, elementInput);
-    };
+    evt.target.style.backgroundColor = window.utils.getArrayRandElement(array);
+    elementInput.value = evt.target.style.backgroundColor;
   };
 
   var toggleElementEvents = function () {
     if (wizardSetup.classList.contains('hidden')) {
-      wizardCoat.removeEventListener('click', onElementPress(wizardCoat, window.utils.WIZARD_COAT_COLORS, inputWizardCoat));
-      fireball.removeEventListener('click', onElementPress(fireballColor, window.utils.FIREBALL_COLORS, inputFireball));
-      wizardEyes.removeEventListener('click', onElementPress(wizardEyes, window.utils.WIZARD_EYE_COLORS, inputWizardEyes));
+      wizardCoat.removeEventListener('click', function (evt) {
+        onElementPress(evt, window.utils.WIZARD_COAT_COLORS, inputWizardCoat);
+      });
+      fireball.removeEventListener('click', function (evt) {
+        onElementPress(evt, window.utils.FIREBALL_COLORS, inputFireball);
+      });
+      wizardEyes.removeEventListener('click', function (evt) {
+        onElementPress(evt, window.utils.WIZARD_EYE_COLORS, inputWizardEyes);
+      });
     }
-    wizardCoat.addEventListener('click', onElementPress(wizardCoat, window.utils.WIZARD_COAT_COLORS, inputWizardCoat));
-    fireball.addEventListener('click', onElementPress(fireballColor, window.utils.FIREBALL_COLORS, inputFireball));
-    wizardEyes.addEventListener('click', onElementPress(wizardEyes, window.utils.WIZARD_EYE_COLORS, inputWizardEyes));
+    wizardCoat.addEventListener('click', function (evt) {
+      onElementPress(evt, window.utils.WIZARD_COAT_COLORS, inputWizardCoat);
+    });
+    fireball.addEventListener('click', function (evt) {
+      onElementPress(evt, window.utils.FIREBALL_COLORS, inputFireball);
+    });
+    wizardEyes.addEventListener('click', function (evt) {
+      onElementPress(evt, window.utils.WIZARD_EYE_COLORS, inputWizardEyes);
+    });
   };
 
   toggleElementEvents();
